d857ca16411e4399d09b74a5651833b5695689e1
pommevilla <pvillanueva13@gmail.com>
2022-12-11 21:50:23 -0600

    Finish day 9...finally...

--- day-11/README.md
+++ day-11/README.md
@@ -3,3 +3,4 @@
 * For Part 2, since we're not dividing worry by 3 every round, the worry levels will get very big very fast over 10,000 rounds. We need to find another way to represent the worry number that still maintains the modular divisibility.
 * There's some modulo arithmetic theorem that says that if `x % a = 0` and `x % b = 0`, then `x % lcm(ab) = 0` (or something, my math is rusty lol). 
 * In other words, after the operations, we can divide the worry by the lcm of all of the monkeys test to still preserve modulo divisibility.
+* There some jankiness with populating the monkey dictionary with who to pass the item to depending on the outcome of the test. I could have made an inner "throw_to" dictionary that had `True` and `False` as the keys to streamline that part of the code.
--- day-9/day-9.py
+++ day-9/day-9.py
@@ -1,36 +1,17 @@
 from typing import List
+from math import copysign
+from time import sleep
 
 
-class knot_position:
-    def __init__(self, x, y) -> None:
-        self.x = x
-        self.y = y
-
+class knot_position(tuple):
     def __add__(self, other) -> "knot_position":
-        return knot_position(self.x + other.x, self.y + other.y)
+        return knot_position(x + y for x, y in zip(self, other))
 
     def __sub__(self, other) -> "knot_position":
-        return knot_position(self.x - other.x, self.y - other.y)
+        return knot_position(x - y for x, y in zip(self, other))
 
-    def __repr__(self) -> str:
-        return f"x={self.x}, y={self.y}"
-
-    def move(self, x, y) -> None:
-        self.x += x
-        self.y += y
-
-    def distance(self, other: "knot_position"):
-        return (self.x - other.x, self.y - other.y)
-
-    def get_position(self):
-        return (self.x, self.y)
-
-    def __contains__(self, key):
-        return key in (self.x, self.y)
-
-    def __iter__(self):
-        for pos in (self.x, self.y):
-            yield pos
+    def distance(self, other) -> "knot_position":
+        return self - other
 
 
 direction_steps = {
@@ -41,47 +22,86 @@ direction_steps = {
 }
 
 
-def part_one(input_file: str) -> int:
-    tail = knot_position(0, 0)
-    head = knot_position(0, 0)
+def next_move(head: knot_position, tail: knot_position) -> knot_position:
+    distance = head - tail
+    if 2 not in distance and -2 not in distance:
+        return knot_position((0, 0))
+    else:
+        move = (0 if x == 0 else (int(copysign(1, x))) for x in distance)
 
-    positions_traveled = set([tail.get_position()])
+        return knot_position(move)
+
+
+def part_one(input_file: str) -> int:
+    tail = knot_position((0, 0))
+    head = knot_position((0, 0))
+
+    places_visited = set()
 
     with open(input_file) as fin:
-        counter = 0
         for line in fin:
-            counter += 1
             direction, steps = line.split()
-            print("-----------------------")
-            print(f"{direction=}, {steps=}")
-            print(f"\tMove: {direction_steps[direction]}")
+            direction = knot_position(direction_steps[direction])
 
-            print(f"\tHead position before move: {head}")
             for _ in range(int(steps)):
+                head = head + direction
 
-                print(f"{head=}\t{tail=}")
-                this_move = direction_steps[direction]
-                head.move(*this_move)
-                if any(abs(coord) == 2 for coord in head.distance(tail)):
-                    if any(abs(coord) == 1 for coord in head.distance(tail)):
-                        # print(f"\tMoving tail with last move: {last_move=}")
-                        tail.move(*last_move)
-                    print(f"\t{head=}\t{tail=}\t{head.distance(tail)}")
-                    tail.move(*this_move)  # This works for one move away
-                    print(f"\tMoving tail with {this_move=}")
+                tail_next_move = next_move(head, tail)
 
-                    positions_traveled.add(tail.get_position())
-                # print(f"Distance={head.distance(tail)}")
-            print(f"\tTail position after move: {tail}")
-            print(f"\tHead position after move: {head}")
-            last_move = this_move
+                tail = tail + tail_next_move
 
-    print(f"{positions_traveled=}")
-    print(f"Num positions visited: {len(positions_traveled)}")
+                places_visited.add(tail)
+
+    print(f"Visited {len(places_visited)} places.")
 
 
-def part_two(input_file: str) -> int:
-    pass
+def show_knots(knots: List[knot_position]) -> None:
+    num_cols = 40
+    num_rows = 28
+    dims = (num_rows, num_cols)
+
+    # manual positioning for test_input_2.txt
+    shift_position = lambda knot: (knot[0] + 14, knot[1] + 10)
+
+    # shift_position = lambda knot: tuple(
+    #     a + int(b / 3) for a, b in zip(knot, dims[::-1])
+    # )
+    shifted_knots = [shift_position(knot) for knot in knots]
+    print("# " * dims[1])
+    for i in range(dims[0]):
+        for j in range(dims[1]):
+            if (j, i) in shifted_knots:
+                print(f"{shifted_knots.index((j, i))} ", end="")
+            else:
+                print("- ", end="")
+        print()
+    print("# " * dims[1])
+
+
+def part_two(input_file: str, num_knots=10) -> int:
+    knots = [knot_position((0, 0)) for _ in range(num_knots)]
+
+    places_visited = set()
+
+    with open(input_file) as fin:
+        for line in fin:
+            direction, steps = line.split()
+            direction = knot_position(direction_steps[direction])
+
+            for _ in range(int(steps)):
+                knots[0] = knots[0] + direction
+                for i in range(len(knots) - 1):
+
+                    next_knot_move = next_move(knots[i], knots[i + 1])
+
+                    knots[i + 1] = knots[i + 1] + next_knot_move
+
+                places_visited.add(knots[-1])
+
+                show_knots(knots)
+                sleep(0.1)
+
+    print(f"Visited {len(places_visited)} places.")
 
 
 def main():
@@ -101,9 +121,10 @@ def main():
 
     elif args.mode.startswith("p"):
         # input_file = "test_input.txt"
-        input_file = "input.txt"
+        input_file = "test_input_2.txt"
+        # input_file = "input.txt"
         if args.mode == "p1":
-            part_one(input_file)
+            part_two(input_file, num_knots=2)
         elif args.mode == "p2":
             part_two(input_file)
 
